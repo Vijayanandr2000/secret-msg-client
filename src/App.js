6582ffb48d107d0f7bc3d67c7a212d1c6456c94e
Vijayanand R <vijaynandr2000@gmail.com>
2021-04-23 11:25:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NewMessage from "./components/NewMessage";
+import DeleteMessage from "./components/DeleteMessage";
+import ShowMessage from "./components/ShowMessage";
+import "./App.css";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => {
+            return <NewMessage />;
+          }}
+        />
+        <Route
+          exact
+          path="/delete"
+          render={(props) => {
+            return <DeleteMessage />;
+          }}
+        />
+        <Route
+          path="/message"
+          render={(props) => {
+            return <ShowMessage />;
+          }}
+        />
+      </Switch>
+    </Router>
   );
 }
 
